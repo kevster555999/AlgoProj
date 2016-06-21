4dbe87355f254c28eb6fbb023552e304fcd831d1
root <root@ip-172-31-31-111.us-west-2.compute.internal>
2016-06-21 19:54:33 +0000

    added html

--- mysite/login/urls.py
+++ mysite/login/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index,name='index'),
+    url(r'^log_in/$', views.log_in, name='log_in'),
+    url(r'^analytics/$', views.analytics, name='analytics'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^projects/$', views.projects, name='projects'),
+
     url(r'(?P<username>[a-zA-Z]+)/$', views.user, name='user'),
 ]
--- mysite/login/views.py
+++ mysite/login/views.py
@@ -4,8 +4,23 @@ from .models import User, Data
 
 
 def index(request):
-    return render(request,'login.view.html')
+    return render(request,'index.html')
+
+def log_in(request):
+    return render(request,'login.html')
+
+def analytics(request):
+    return render(request,'analytics.html')
+
+
+def contact(request):
+    x = "Name: Kevin Huang \nEmail: kh522@cornell.edu"
+    return HttpResponse(x)
+
+def projects(request):
+    return HttpResponse("Projects will go here")
+
 
 def user(request, username):
-    return HttpResponse("Hello World from: %s." % username)
+    return HttpResponse("Invalid Link: %s." % username)
 
